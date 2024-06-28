ad5553304f8e6b8fd0e829d3b0abbef7396723e1
rasmusskriver <rasmus_skriver@yahoo.dk>
2024-06-29 00:59:49 +0200

    etc

--- game.js
+++ game.js
@@ -19,28 +19,6 @@ function gameLoop() {
 	ctx.strokeStyle = 'rgb(0,128,0)';
 	ctx.lineWidth = 1;
 	ctx.stroke();
-	// Tegn tårnet
-	// Tegn tårnens krop
-	//    ctx.fillStyle = '#8B4513'; // Brun farve
-	//    ctx.fillRect(100, 150, 50, 150); // Rektangulær krop
-	//
-	//    // Tegn tårnets top
-	//    ctx.fillStyle = '#CD853F'; // Lysere brun farve
-	//    ctx.fillRect(75, 100, 100, 50); // Rektangulær top
-	//
-	//    // Tegn vindue
-	//    ctx.fillStyle = '#ADD8E6'; // Blå farve
-	//    ctx.fillRect(90, 160, 20, 20); // Vindue
-	//	ctx.fillStyle = '#ccddff';
-	//	ctx.beginPath();
-	//	ctx.moveTo(60,60);
-	//	ctx.lineTo(100,50);
-	//	ctx.lineTo(150,80);
-	//	ctx.closePath();
-	//	ctx.fill();
-	//	ctx.strokeStyle = 'rgb(0,128,0)';
-	//	ctx.lineWidth = 1;
-	//	ctx.stroke();
 	// få fat i næste frame
 	requestAnimationFrame(gameLoop);
 }
